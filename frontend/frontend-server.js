be869c3203333891389ae48a756cbec9b7f1d974
davila1292 <daap1292@gmail.com>
2025-12-05 22:16:19 +0000

    new

--- frontend/frontend-server.js
+++ frontend/frontend-server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
     console.log(`Frontend server listening on port ${port}`);
 });
